6dbc14b0dfcbebee687d0ff7d5b41e8df621fa4d
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-11-10 22:11:53 +0300

    Добавьте файлы проекта.

---
+++ List/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Sake = new List<string>  { "Екатерина", "Виктория", "Виктор", "Андрей", "Аня" };
+            Console.WriteLine(Name(Sake));
+            Console.ReadKey();
+        }
+        public static int Name(List<string> number)
+        {
+            int n = 0;
+            for (int i = 0; i < number.Count; i++)
+            {
+                if (number[i].Length >= 7)
+                {
+                    n++;
+                }
+            }
+            return n;
+        }
+    }
+}
